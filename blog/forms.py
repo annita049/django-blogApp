8a903d4a914aff1c8870774f296c0eb12f181921
annita049 <e.annita2001@gmail.com>
2024-12-29 14:57:57 +0600

    define BlogForm metadata and no photo condition

--- blog/forms.py
+++ blog/forms.py
@@ -5,3 +5,9 @@ class BlogForm(forms.ModelForm):
     class Meta:
         model = Blog
         fields = ['text', 'photo']
+    def clean_photo(self):
+    # If no photo is uploaded -> we return default image of no photo
+        photo = self.cleaned_data.get('photo')
+        if not photo:
+            return None  # empty photo field
+        return photo
