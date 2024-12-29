e24d369985d18da809e450feffa29c12eec622a0
annita049 <e.annita2001@gmail.com>
2024-12-29 14:58:39 +0600

    added default image in case of no image

--- blog/models.py
+++ blog/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class Blog(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     text = models.TextField(max_length=240)
-    photo = models.ImageField(upload_to='photos/', blank=True, null=True)
+    photo = models.ImageField(upload_to='photos/', blank=True, null=True, default='photos/pexels-no_photo.jpg')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
