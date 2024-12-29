12f09dd559144740a1e866e677659143fa5c111a
annita049 <e.annita2001@gmail.com>
2024-12-29 14:59:13 +0600

    added create, edit, update urls in the blog app

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.All_Blogs, name='all_blogs'),
+    path('create/', views.Create_Blog, name='Create_Blog'),
+    path('<int:blog_id>/edit/', views.Edit_Blog, name='Edit_Blog'),
+    path('<int:blog_id>/delete/', views.Delete_Blog, name='Delete_Blog'),
 ]
