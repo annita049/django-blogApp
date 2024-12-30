6b77769bdcbea4e6ca8fba013b9ca8791b4dd051
annita049 <e.annita2001@gmail.com>
2024-12-31 01:41:45 +0600

    some styling stuff and added few templates and linked with navbar menu

--- blog/templatetags/rounded_time.py
+++ blog/templatetags/rounded_time.py
@@ -4,5 +4,15 @@ from blog.utils import rounded_time_ago  # Adjust the import path as needed
 register = template.Library()
 
 @register.filter
-def rounded_time(value):
+def rounded_timesince(value):
     return rounded_time_ago(value)
+
+@register.filter
+def formatted_date(value):
+    """
+    This custom filter formats the date as '24 March, 2023'.
+    """
+    if not value:
+        return value
+    
+    return value.strftime('%d %B, %Y')
