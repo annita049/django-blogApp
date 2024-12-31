9f03fd1c60f6347e4bad4cbb44e30ccc8f5cc623
annita049 <e.annita2001@gmail.com>
2024-12-31 22:10:07 +0600

    registered profile model to admin site

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from .models import Blog
+from .models import Profile
 # Register your models here.
 
 admin.site.register(Blog)
+
+admin.site.register(Profile)
+
+# @admin.register(Profile)
+# class ProfileAdmin(admin.ModelAdmin):
+#     list_display = ('user', 'profile_picture')
